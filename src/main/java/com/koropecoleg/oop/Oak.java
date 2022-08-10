8af24480204274113f0709a57a9644aed5766e14
Oleh Koropets <k.olegu23@gmail.com>
2022-08-10 12:12:28 +0300

    Hi

--- src/main/java/com/koropecoleg/oop/Oak.java
+++ src/main/java/com/koropecoleg/oop/Oak.java
@@ -1,20 +1,29 @@
 package com.koropecoleg.oop;
 
 public class Oak extends Plants {
-    int  height_of_plant = 50;
-    int  width_of_plant = 15;
+    int  height_of_plant;
+    int  width_of_plant;
+
+    Oak(int height_of_plant, int width_of_plant) {
+        this.height_of_plant = height_of_plant;
+        this.width_of_plant = width_of_plant;
+    }
+
+    Oak(){
+    }
+
     protected  void blossom(){
         System.out.println("I blossom in spring");
     }
 
-    protected void height(double height_of_plant, double width_of_plant) {
-        height_of_plant = this.height_of_plant + height_of_plant;
-        width_of_plant = this.width_of_plant + width_of_plant;
-        System.out.println("Oak height is now " + height_of_plant + "m and width is now " + width_of_plant );
+    protected void height(double height, double width) {
+        height = height_of_plant + height;
+        width = width_of_plant + width;
+        System.out.println("Oak height is now " + height + "m and width is now " + width );
     }
 
     public static void main(String[] args) {
-        Oak oak = new Oak();
+        Oak oak = new Oak(50, 15);
         oak.blossom();
         oak.height(0.3, 0.15);
 
